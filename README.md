40b7c00061f0f7ba84d50f2e1dbad290267b2cf0
TheSlam <32799244+salmmanfred@users.noreply.github.com>
2021-07-24 00:06:30 +0200

    2.0.0

--- README.md
+++ README.md
@@ -1,16 +1,13 @@
-# This is a cargo for easy opening of files.
+# This is a cargo for easy opening of files.  
 
-To get the file into a string: readFile(&str)  Returns string    
-
-To get files line by line readFileLines(&str) Retruns vector string
-
-To write to a file do : writeFile(&str,&str) (file name, file content) Returns i8(1 if success)  
-
-To Transfer a file (or copy a file from one file to another)  use: fileTransfer(&str,&str) (The file that the contents will be dumped to)
-(the file its copying from) returns the i8 results from writeFile  
-
-To swap files you need to do: fileSwap(&str,&str) the 2 strings are the 2 files that are being changed Returns 2 if success 2 being the combined value from writefile returns i8  
-
-To delete file removeFile(&str) to remove the file using the name of the file
-
-To save byte arrays do: writeFileBytes(&str,Vec u8)
+To get the file into a string: read_file(&str)  Returns string  
+  
+To get files line by line read_file_lines(&str) Retruns vector string  
+  
+To write to a file do : write_file(&str,&str) (file name, file content)  
+  
+To delete file remove_file(&str) to remove the file using the name of the file  
+  
+To save byte arrays do: write_file_bytes(&str,Vec u8)  
+  
+read_file_bytes (&str: filename)  
--- src/lib.rs
+++ src/lib.rs
@@ -3,27 +3,92 @@
 * MIT license
 */
 
-pub mod filehand;
-pub mod filetransfer;
+/*
+* Made by salmmanfred 2021/01/13
+* MIT license
+*/
 
-pub fn readFile(filename: &str) -> String{
-    return filehand::readFile(filename);
+use std::fs::File;
+use std::io::{prelude::*,BufReader};
+use std::path::Path;
+use std::fs;
+
+pub fn remove_file(fnm:&str) -> std::io::Result<()> {
+    fs::remove_file(fnm)?;
+    Ok(())
 }
-pub fn readFileLines(filename: &str) -> Vec<String>{
-    return filehand::readFileLines(filename);
+
+pub fn read_file(names: &str) -> String {
+    //reads the file from names
+    let s = "".to_string();
+    let fnm = s+&names;
+    let contents =  std::fs::read_to_string(fnm).unwrap();
+    return contents;
+
+    
 }
-pub fn writeFile(filename: &str,content: &str) -> i8{
-    return filehand::writeFile(filename,content)
+
+
+
+pub fn write_file(names:&str, cont:&str) -> std::io::Result<()> {
+    // writes to a file
+    let s = "".to_string();
+    let fnm = s+&names;
+    let path = Path::new(&fnm);
+    
+
+    // Open a file in write-only mode, returns `io::Result<File>`
+    let mut file = File::create(&path)?;
+
+    // Write the `LOREM_IPSUM` string to `file`, returns `io::Result<()>`
+    file.write_all(cont.as_bytes())?;
+    
+    Ok(())
+    
+
+    
+    
+
 }
-pub fn fileTransfer(filename: &str,content: &str) -> i8{
-    return filetransfer::fileTransfer(filename, content)
+pub fn read_file_lines(fnm:&str) -> Vec<String> {
+    // reads the file line by line
+    let x: &str = &fnm;
+    let mut errcode = "Error".to_owned();
+    errcode.push_str(x);
+    let file = File::open(x).expect(&errcode);
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect()
 }
-pub fn fileSwap(filename: &str,content: &str) -> i8{
-    return filetransfer::fileSwap(filename, content)
+
+pub fn write_file_bytes(names:&str, cont:Vec<u8>) -> std::io::Result<()> {
+    // writes to a file
+    let s = "".to_string();
+    let fnm = s+&names;
+    let path = Path::new(&fnm);
+    
+
+    // Open a file in write-only mode, returns `io::Result<File>`
+    let mut file = File::create(&path)?;
+    //use std::io::Cursor;
+
+    // Write the `LOREM_IPSUM` string to `file`, returns `io::Result<()>`
+    for x in cont{
+        file.write(&[x])?;
+    }
+    Ok(())
+
+    
+
 }
-pub fn removeFile(filename: &str)-> std::io::Result<()>{
-    return filehand::removeFile(filename);
+pub fn read_file_bytes(filename: &str) -> Vec<u8> {
+    let mut f = File::open(&filename).expect("no file found");
+    let metadata = fs::metadata(&filename).expect("unable to read metadata");
+    let mut buffer = vec![0; metadata.len() as usize];
+    f.read(&mut buffer).expect("buffer overflow");
+
+    buffer
 }
-pub fn writeFileBytes(names:&str, cont:Vec<u8>){
-    filehand::writeFileBytes(names, cont);
-}
+
+
