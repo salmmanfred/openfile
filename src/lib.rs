da146167050e90bd41c91b34889e1e9e36bba920
salmmanfred <32799244+salmmanfred@users.noreply.github.com>
2023-01-15 00:04:18 +0100

    Clean up

--- src/lib.rs
+++ src/lib.rs
@@ -1,85 +1,58 @@
 /*
-* Made by salmmanfred 2021/01/13
+* Made by salmmanfred 2023/01/14
 * MIT license
 */
 
-/*
-* Made by salmmanfred 2021/01/13
-* MIT license
-*/
-
-use std::fs::File;
-use std::io::{prelude::*,BufReader};
-use std::path::Path;
 use std::fs;
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+use std::path::Path;
 
-pub fn remove_file(fnm:&str) -> std::io::Result<()> {
+pub fn remove_file(fnm: &str) -> std::io::Result<()> {
     fs::remove_file(fnm)?;
     Ok(())
 }
 
 pub fn read_file(names: &str) -> std::io::Result<String> {
-    //reads the file from names
-    let contents =  std::fs::read_to_string(names)?;
+    let contents = std::fs::read_to_string(names)?;
     Ok(contents)
-
-    
 }
 
-
-
-pub fn write_file(names:&str, cont:&str) -> std::io::Result<()> {
-    // writes to a file
-    let s = "".to_string();
-    let fnm = s+&names;
-    let path = Path::new(&fnm);
-    
-
-    // Open a file in write-only mode, returns `io::Result<File>`
+pub fn write_file(names: &str, cont: &str) -> std::io::Result<()> {
+    let path = Path::new(names);
     let mut file = File::create(&path)?;
-
-    // Write the `LOREM_IPSUM` string to `file`, returns `io::Result<()>`
     file.write_all(cont.as_bytes())?;
-    
     Ok(())
-    
-
-    
-    
-
 }
-pub fn read_file_lines(fnm:&str) -> std::io::Result<Vec<String>> {
+pub fn read_file_lines(fnm: &str) -> std::io::Result<Vec<String>> {
     // reads the file line by line
     let x: &str = &fnm;
     let mut errcode = "Error".to_owned();
     errcode.push_str(x);
     let file = File::open(x)?;
     let buf = BufReader::new(file);
-    let buf: Vec<String> = buf.lines()
+    let buf: Vec<String> = buf
+        .lines()
         .map(|l| l.expect("Could not parse line"))
         .collect();
     Ok(buf)
 }
 
-pub fn write_file_bytes(names:&str, cont:Vec<u8>) -> std::io::Result<()> {
+pub fn write_file_bytes(names: &str, cont: Vec<u8>) -> std::io::Result<()> {
     // writes to a file
     let s = "".to_string();
-    let fnm = s+&names;
+    let fnm = s + &names;
     let path = Path::new(&fnm);
-    
 
     // Open a file in write-only mode, returns `io::Result<File>`
     let mut file = File::create(&path)?;
     //use std::io::Cursor;
 
     // Write the `LOREM_IPSUM` string to `file`, returns `io::Result<()>`
-    for x in cont{
+    for x in cont {
         file.write(&[x])?;
     }
     Ok(())
-
-    
-
 }
 pub fn read_file_bytes(filename: &str) -> std::io::Result<Vec<u8>> {
     let mut f = File::open(&filename)?;
@@ -90,30 +63,29 @@ pub fn read_file_bytes(filename: &str) -> std::io::Result<Vec<u8>> {
     Ok(buffer)
 }
 
-
-pub trait AsFile{
+pub trait AsFile {
     fn as_file(&self, filename: &str) -> std::io::Result<()>;
 }
-impl AsFile for &[u8]{
-    fn as_file(&self, filename: &str) -> std::io::Result<()>{
+impl AsFile for &[u8] {
+    fn as_file(&self, filename: &str) -> std::io::Result<()> {
         write_file_bytes(filename, self.to_vec())?;
         Ok(())
     }
 }
-impl AsFile for Vec<u8>{
-    fn as_file(&self, filename: &str) -> std::io::Result<()>{
+impl AsFile for Vec<u8> {
+    fn as_file(&self, filename: &str) -> std::io::Result<()> {
         write_file_bytes(filename, self.to_owned())?;
         Ok(())
     }
 }
-impl AsFile for String{
-    fn as_file(&self, filename: &str) -> std::io::Result<()>{
+impl AsFile for String {
+    fn as_file(&self, filename: &str) -> std::io::Result<()> {
         write_file(filename, self.as_str())?;
         Ok(())
     }
 }
-impl AsFile for str{
-    fn as_file(&self, filename: &str) -> std::io::Result<()>{
+impl AsFile for str {
+    fn as_file(&self, filename: &str) -> std::io::Result<()> {
         write_file(filename, self)?;
         Ok(())
     }
